c8b6c66216f2371a24bca2e4cbe481b1a67d9eef
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-12-14 17:36:03 +0100

    configured eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Main page</div>;
+  return <div className='App'>Main page</div>;
 }
 
 export default App;
