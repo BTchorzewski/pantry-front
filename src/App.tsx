c7054c86e0b41dad597ad89256131c6322a095a7
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-12-14 21:34:54 +0100

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Navigation } from './components/Navigation/Navigation';
 
 function App() {
-  return <div className='App'>Main page</div>;
+  return (
+    <div className='App'>
+      <Navigation />
+      Main page
+    </div>
+  );
 }
 
 export default App;
