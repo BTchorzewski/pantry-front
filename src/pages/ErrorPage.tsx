5b39da3cf0b554f52a880751a7106f4cbb0775fe
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-12-15 13:32:43 +0100

    added error page.

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import { useRouteError } from 'react-router-dom';
+import { Navigation } from '../components/Navigation/Navigation';
+
+interface ErrorType {
+  status: number;
+  message: string;
+  statusText: string;
+  internal: boolean;
+  data: string;
+  error: unknown;
+}
+
+export default function ErrorPage() {
+  const error = useRouteError() as ErrorType;
+  console.log(JSON.stringify(error));
+  return (
+    <div id='error-page'>
+      <Navigation />
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText !== '' || error.message}</i>
+      </p>
+    </div>
+  );
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import { LoginPage } from '../pages/LoginPage';
+import ErrorPage from '../pages/ErrorPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
