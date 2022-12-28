bd5791deaca4e3c038d6a8bcea638633339153f8
BTchorzewski <tchorzewskibartosz@gmail.com>
2022-12-28 15:19:45 +0100

    kicking off an interceptor.

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,16 +1,38 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
+import { TokensRes } from '../types';
 
 export const basicRoute = axios.create({
-  baseURL: 'http://localhost:3001',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-export const protectedBasicRoute = axios.create({
   baseURL: 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
     withCredentials: true,
   },
 });
+
+const protectedRoute = axios.create({
+  baseURL: 'http://localhost:3001',
+  headers: {
+    'Content-Type': 'application/json',
+    withCredentials: true,
+  },
+});
+
+export const protectedBasicRoute = protectedRoute.interceptors.response.use(
+  (res) => {
+    console.log('fetch api');
+    return res;
+  },
+  async (error: unknown): Promise<AxiosError> => {
+    if (error instanceof AxiosError) {
+      console.log({ 'interceptor status': error.status });
+    }
+    return Promise.reject(error);
+  }
+) as unknown as AxiosInstance;
+
+export const refreshToken = async (): Promise<string | null> => {
+  const results = await basicRoute.get('/auth/refresh-token');
+  if (results.status !== 200) return null;
+  const { accessToken }: TokensRes = await results.data;
+  return accessToken;
+};
