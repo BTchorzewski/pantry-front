453fc7cee3c079881b0338e7873d45ac38287ac4
BTchorzewski <tchorzewskibartosz@gmail.com>
2023-01-11 13:25:01 +0100

    reformated code

--- src/hooks/useRefreshToken.tsx
+++ src/hooks/useRefreshToken.tsx
@@ -1,26 +1,25 @@
-import {useToken} from './useToken';
-import {basicRoute, protectedBasicRoute} from '../utils/fetch';
-import {TokensRes} from '../types';
-import {AxiosResponse} from 'axios';
+import { useToken } from './useToken';
+import { basicRoute, protectedBasicRoute } from '../utils/fetch';
+import { TokensRes } from '../types';
+import { AxiosResponse } from 'axios';
 
 type UseRefreshTokenReturn = () => void;
 
 export const UseRefreshToken = (): UseRefreshTokenReturn => {
-    const [token, setToken] = useToken();
-    const refreshToken = async () => {
-        try {
-            const {data} = await protectedBasicRoute.get('/auth/refresh-token');
-            const {accessToken} = data as TokensRes;
-            console.log({
-                token,
-                accessToken,
-            });
-            setToken(accessToken);
+  const [token, setToken] = useToken();
+  const refreshToken = async () => {
+    try {
+      const { data } = await basicRoute.get('/auth/refresh-token');
+      const { accessToken } = data as TokensRes;
+      console.log({
+        token,
+        accessToken,
+      });
+      setToken(accessToken);
+    } catch (e) {
+      console.log({ refreshError: e });
+    }
+  };
 
-        } catch (e) {
-            console.log({refreshError: e})
-        }
-    };
-
-    return refreshToken;
+  return refreshToken;
 };
