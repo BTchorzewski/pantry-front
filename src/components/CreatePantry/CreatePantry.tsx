7bc75b3cefc092841b48b36fe08913e7890b38a8
BTchorzewski <tchorzewskibartosz@gmail.com>
2023-01-19 16:51:49 +0100

    moved the form to an independent component.

--- src/components/CreatePantry/CreatePantry.tsx
+++ src/components/CreatePantry/CreatePantry.tsx
@@ -1,74 +1,13 @@
 import './CreatePantry.css';
-import { useToken } from '../../hooks/useToken';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { basicRoute, protectedBasicRoute } from '../../utils/fetch';
-import { AxiosError } from 'axios';
-import { CreatePantryResponse, ShortPantry } from '../../types';
-import { useNavigate } from 'react-router-dom';
-import { useRefreshToken } from '../../hooks/useRefreshToken';
 import { usePantries } from '../../hooks/usePantries';
-type SetPantries = (pantries: ShortPantry[]) => ShortPantry[];
-
+import { CreatePantryForm } from '../Forms/CreatePantryForm/CreatePantryForm';
 export const CreatePantry = () => {
-  const { addPantry } = usePantries();
-  const [token, setToken] = useToken();
-  const [name, setName] = useState('');
-  const [result, setResult] = useState(false);
-  const navigation = useNavigate();
-  const refreshToken = useRefreshToken();
-  const createPantry = async (e: FormEvent): Promise<void> => {
-    e.preventDefault();
-    try {
-      const config = {
-        headers: {
-          // prettier-ignore
-          'Authorization': `Bearer ${token}`,
-        },
-      };
-      const results = await basicRoute.post('/pantry', { name }, config);
-
-      const { pantryId: id } = results.data as CreatePantryResponse;
-      if (id !== undefined) {
-        addPantry({
-          name,
-          id,
-          stats: {
-            total: 0,
-            fresh: 0,
-            expiredSoon: 0,
-            expired: 0,
-          },
-        });
-      }
-      setName('');
-    } catch (e: unknown) {
-      if (e instanceof AxiosError) {
-        if (e.response?.status === 401) {
-          await refreshToken();
-          //  @todo create hooks for pantries management.
-        }
-      }
-    }
-  };
-  const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
-    setName(e.target.value);
-  };
-
+  const { pantries } = usePantries();
   return (
     <div className='CreatePantry'>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
-      <form onSubmit={createPantry} className='CreatePantry__form'>
-        <label className='CreatePantry__label'>
-          Name:
-          <input
-            className='CreatePantry__input'
-            type='text'
-            value={name}
-            onChange={handleInput}
-          />
-        </label>
-      </form>
-      {result ? <p>Pantry has been created.</p> : null}
+      Create pantry :)
+      <CreatePantryForm />
+      {pantries?.length ? <p>Pantry has been created.</p> : null}
     </div>
   );
 };
---
+++ src/components/Forms/CreatePantryForm/CreatePantryForm.tsx
@@ -0,0 +1,59 @@
+import './CreatePantryForm.css';
+import { FormEvent, useRef } from 'react';
+import { basicRoute } from '../../../utils/fetch';
+import { CreatePantryResponse } from '../../../types';
+import { AxiosError } from 'axios';
+import { usePantries } from '../../../hooks/usePantries';
+import { useBearerToken } from '../../../hooks/useBearerToken';
+import { useRefreshToken } from '../../../hooks/useRefreshToken';
+
+export const CreatePantryForm = () => {
+  const { addPantry } = usePantries();
+  const bearer = useBearerToken();
+  const refreshToken = useRefreshToken();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const createPantry = async (e: FormEvent): Promise<void> => {
+    e.preventDefault();
+    try {
+      if (nameRef?.current?.value) {
+        const results = await basicRoute.post(
+          '/pantry',
+          { name: nameRef.current.value },
+          bearer
+        );
+
+        const { pantryId: id } = results.data as CreatePantryResponse;
+
+        if (id !== undefined) {
+          addPantry({
+            id,
+            name: nameRef.current.value,
+            stats: {
+              total: 0,
+              fresh: 0,
+              expiredSoon: 0,
+              expired: 0,
+            },
+          });
+        }
+      }
+    } catch (e: unknown) {
+      if (e instanceof AxiosError) {
+        if (e.response?.status === 401) {
+          await refreshToken();
+          //  @todo create hooks for pantries management.
+        }
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={createPantry} className='CreatePantry__form'>
+      <label className='CreatePantry__label'>
+        Name:
+        <input className='CreatePantry__input' type='text' ref={nameRef} />
+      </label>
+      <button type={'submit'}>Create pantry</button>
+    </form>
+  );
+};
